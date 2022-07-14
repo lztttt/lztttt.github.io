46303dd5f2f834e64cb0c3dc5c6714f08aebf0a3
lztttt <1975677818@qq.com>
2022-07-14 20:45:12 +0800

    1

--- assets/js/main.js
+++ assets/js/main.js
@@ -59,16 +59,16 @@ $(document).ready(function () {
 	}
 });
 
-$('.btn-mobile-menu__icon').click(function () {
-	if ($('.navigation-wrapper').css('display') == "block") {
-		$('.navigation-wrapper').on('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function () {
-			$('.navigation-wrapper').toggleClass('visible animated bounceOutUp');
-			$('.navigation-wrapper').off('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend');
-		});
-		$('.navigation-wrapper').toggleClass('animated bounceInDown animated bounceOutUp');
+// $('.btn-mobile-menu__icon').click(function () {
+// 	if ($('.navigation-wrapper').css('display') == "block") {
+// 		$('.navigation-wrapper').on('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function () {
+// 			$('.navigation-wrapper').toggleClass('visible animated bounceOutUp');
+// 			$('.navigation-wrapper').off('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend');
+// 		});
+// 		$('.navigation-wrapper').toggleClass('animated bounceInDown animated bounceOutUp');
 
-	} else {
-		$('.navigation-wrapper').toggleClass('visible animated bounceInDown');
-	}
-	$('.btn-mobile-menu__icon').toggleClass('social iconfont icon-list social iconfont icon-angleup animated fadeIn');
-});
+// 	} else {
+// 		$('.navigation-wrapper').toggleClass('visible animated bounceInDown');
+// 	}
+// 	$('.btn-mobile-menu__icon').toggleClass('social iconfont icon-list social iconfont icon-angleup animated fadeIn');
+// });
